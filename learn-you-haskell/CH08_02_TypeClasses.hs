79691097580961fa3c2dc43b556d4f7901a87750
Igodlab <igodlab@gmail.com>
2021-08-23 12:02:56 -0700

    Chapter 8 finished: Typeclasses, instances & kinds

--- learn-you-haskell/CH08_02_TypeClasses.hs
+++ learn-you-haskell/CH08_02_TypeClasses.hs
@@ -29,37 +29,60 @@ instance Show TrafficLight where
 --
 
 class YesNo a where
-    yesNo :: a -> Bool
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
 
 instance YesNo [a] where
-    yesNo [] = False
-    yesNo _  = True 
+    yesno [] = False
+    yesno _  = True 
 
 -- instances take a type constructor that is concrete (e.g. Maybe a)
 
 instance YesNo (Maybe a) where  
-    yesNo (Just _) = True
-    yesNo Nothing  = False
+    yesno (Just _) = True
+    yesno Nothing  = False
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 
 instance YesNo (Tree a) where
-    yesNo Empty = False
-    yesNo _     = True
+    yesno Empty = False
+    yesno _     = True
 
 instance YesNo TrafficLight where
-    yesNo Red = False
-    yesNo _   = True
+    yesno Red = False
+    yesno _   = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
 
 -- -------------------------------------------------------------------                     
 -- ------------------------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------------------
--- 8.8 YES NO TYPECLASs (pg. 112)
+-- 8.9 FUNCTOR TYPECLAS (pg. 112)
 -- Functor typeclass is for things that can be mapped over
 -- 
 
+-- Functors take a type that has a type constructor (not concrete, this is the f)
+-- Say we have written an instance for the typeclass Functor as
+--
+-- instance Functor Maybe where
+--      fmap f (Just a) = Just (f a)
+--      fmap f Nothing  = Nothing
+--
+-- Notice how we wrote instance Functor Maybe where instead of instance Functor (Maybe m) where, like we did when we were dealing with Maybe and YesNo (above).
+-- Functor wants a type constructor that takes one type and not a concrete type. If you mentally replace the fs with Maybes, fmap acts like a 
+-- 
+-- (a -> b) -> f     a -> f     b
+--
+-- (a -> b) -> Maybe a -> Maybe b
+--
+-- for this particular type, which looks OK. But if you replace f with (Maybe m), then it would seem to act like a `(a -> b) -> Maybe m a -> Maybe m b`, which doesn't make any damn sense because Maybe takes just one type parameter.
 
--- Functors take a type that has a type constructor (not concrete)
+-- example 1, with trees
+--
 instance Functor Tree where
     fmap f Empty = Empty
     fmap f (Node x left rigth) = Node (f x) (fmap f left) (fmap f rigth)
@@ -72,4 +95,54 @@ treeInsert x (Node y left right)
     | x == y = Node x left right
     | x > y  = Node y left (treeInsert x right)
     | x < y  = Node y (treeInsert x left) right 
+
+rConvert :: (Num a, Ord a) => Tree a -> a -> Either a a
+rConvert (Node x lft rgt) y
+    | x > y = Right x
+    | otherwise = Left x
+
+-- now try in ghci:
+-- λ> let tree1 = foldr treeInsert (Node 3 Empty Empty) [4,2,7,8]
+-- λ> fmap (*2) tree1
+--
+-- -------------------------------------------------------------------                     
+-- ------------------------------------------------------------------------------------------
+-- ------------------------------------------------------------------------------------------
+-- 8.10 KINDS & SOME TYPE FOO (pg. 115)
+-- Functor typeclass is for things that can be mapped over
+-- 
+-- some weird examples with kkinds:
+
+-- example 1
+class Tofu t where
+    -- | tofu takes `j a` where a is a concrete type *, so we infer that j is * -> *
+    --   then, `t a j` has to be * -> (* -> *) -> *
+    tofu :: j a -> t a j
+
+data Frank a b = Frank {frankField :: b a} 
+    deriving Show
+  
+-- try in ghci
+-- λ> :t Frank {frankField = Just "hahaha"}
+-- λ> :t Frank {frankField = Node 'a' Empty Empty}
+
+
+instance Tofu Frank where
+    -- | now make Frank an instance of Tofu
+    tofu x = Frank x
+
+-- try in ghci
+-- λ> tofu (Just 'a') :: Frank Char Maybe
+-- λ> tofu ["HELLO"] :: Frank [Char] []
+--
+
+-- example 2
+data Pedro t k p = Pedro {yaba :: p, daba :: t k}
+
+instance Functor (Pedro a b) where
+    -- | make Pedro an instance of Functor, Functor takes kinds (* -> *) -> Constraints
+    --   and Pedro has only p as a concrete constraint *, and returns a concrete kind as well
+    --   Pedro kinds: something -> something -> * -> *
+    fmap f (Pedro {yaba = x, daba = y}) = Pedro {yaba = f x, daba = y}
+
 
