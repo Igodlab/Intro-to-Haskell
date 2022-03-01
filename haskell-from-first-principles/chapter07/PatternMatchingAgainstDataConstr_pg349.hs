99975a8865f7c84a523ff05b50403988f6ab7eb2
Igodlab <igodlab@gmail.com>
2022-03-01 15:44:12 -0800

    early chapter book examples

--- haskell-from-first-principles/chapter07/PatternMatchingAgainstDataConstr_pg349.hs
+++ haskell-from-first-principles/chapter07/PatternMatchingAgainstDataConstr_pg349.hs
@@ -9,3 +9,14 @@ data User = UnregisteredUser | RegisteredUser Username AccountNumber
 printUser :: User -> IO ()
 printUser UnregisteredUser = putStrLn "UnregisteredUser"
 printUser (RegisteredUser (Username n) (AccountNumber a)) = putStrLn $ n ++ " - " ++ show a
+
+addOneIfOdd n = case odd n of
+                True  -> f n
+                False -> n
+              -- where f n = n + 1
+              -- # Ans:
+              where f = \x -> x + 1
+                
+addFive = \x -> \y -> 5 + if x > y then y else x 
+
+mflip f = \x -> \y -> f y x
