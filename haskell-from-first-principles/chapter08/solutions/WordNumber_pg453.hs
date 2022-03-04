766523b9cae75f5166f644a801cd85fb85837d39
Igodlab <igodlab@gmail.com>
2022-03-04 10:02:16 -0800

    chapter 8 improved solution

--- haskell-from-first-principles/chapter08/solutions/WordNumber_pg453.hs
+++ haskell-from-first-principles/chapter08/solutions/WordNumber_pg453.hs
@@ -1,16 +1,27 @@
 module WordNumber where
 
-import Data.List (intersperse)
+import Data.List (intercalate)
 
 digitToWord :: Int -> String
 -- digitToWord n = undefined
-digitToWord n = take n ['a'..]
+digitToWord = str 
+  where
+    str 0 = "zero"
+    str 1 = "one"
+    str 2 = "two"
+    str 3 = "three"
+    str 4 = "four"
+    str 5 = "five"
+    str 6 = "six"
+    str 7 = "seven"
+    str 8 = "eight"
+    str 9 = "nine"
 
 digits :: Int -> [Int]
 -- digits n = undefined
 digits 0 = []
-digits x = x : digits (x -1)
+digits n = digits (div n 10) ++ [mod n 10]
 
 wordNumber :: Int -> String
 -- wordNumber n = undefined
-wordNumber n = take n ['a'..]
+wordNumber = intercalate " - " . map digitToWord . digits
