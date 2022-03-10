5a6ba7538d1aeb1d2ec5d9b2989779e938e7a263
Igodlab <igodlab@gmail.com>
2022-03-10 11:39:44 -0800

    Database exercises cleaned & done

--- haskell-from-first-principles/chapter10/solutions/DatabaseProcessing_pg566.hs
+++ haskell-from-first-principles/chapter10/solutions/DatabaseProcessing_pg566.hs
@@ -11,18 +11,24 @@ theDatabase :: [DatabaseItem]
 theDatabase = [ DbDate (DT.UTCTime (DT.fromGregorian 1911 5 1) (DT.secondsToDiffTime 34123))
                 , DbNumber 9001
                 , DbString "Hello, world!"
+                , DbNumber 133
                 , DbDate (DT.UTCTime (DT.fromGregorian 1921 5 1) (DT.secondsToDiffTime 34123))
               ]
 
 -- 1. Write a function that filters for DbDate values and returns a list of the DT.UTCTime values inside them.
---
 filterDbDate :: [DatabaseItem] -> [DT.UTCTime]
 filterDbDate xs = [i | (DbDate i) <- xs]
+
+filterDbDate' :: [DatabaseItem] -> [DT.UTCTime]
+filterDbDate' [] = []
+filterDbDate' (x:xs) = case x of 
+                        (DbDate i) -> i : filterDbDate' xs
+                        _          -> filterDbDate' xs
 
 
 -- 2. Write a function that filters for DbNumber values and returns a list of the Integer values inside them. 
 filterDbNumber :: [DatabaseItem] -> [Integer]
-filterDbNumber xs = [i | (DbNumber) <- xs]
+filterDbNumber xs = [i | (DbNumber i) <- xs]
 
 
 -- 3. Write a function that gets the most recent date. 
@@ -38,6 +44,8 @@ sumDb = sum . filterDbNumber
 -- You'll probably need to use fromIntegral
 -- to get from Integer to Double.
 avgDb :: [DatabaseItem] -> Double
-avgDb xs = (/) ((sum . numInt) xs) ((length . numInt) xs) 
+avgDb xs = (/) (sumX xs) (nX xs)
   where
-    numInt = fromIntegral . filterDbNumber
+    sumX = fromIntegral . sum . filterDbNumber
+    nX   = fromIntegral . length . filterDbNumber
+
