0b856c63f740e1a421fd7bbcf1f94eda20e6e895
Igodlab <igodlab@gmail.com>
2022-03-09 23:59:36 -0800

    progress on database

--- haskell-from-first-principles/chapter10/solutions/DatabaseProcessing_pg566.hs
+++ haskell-from-first-principles/chapter10/solutions/DatabaseProcessing_pg566.hs
@@ -4,38 +4,40 @@ import qualified Data.Time as DT
 
 data DatabaseItem = DbString String
                   | DbNumber Integer
-                  | DbDate UTCtime
+                  | DbDate DT.UTCTime
                   deriving (Eq, Ord, Show)
 
 theDatabase :: [DatabaseItem]
-theDatabase = [ DbDate (UTCTime (DT.fromGregorian 1911 5 1) (DT.secondsToDiffTime 34123))
+theDatabase = [ DbDate (DT.UTCTime (DT.fromGregorian 1911 5 1) (DT.secondsToDiffTime 34123))
                 , DbNumber 9001
                 , DbString "Hello, world!"
-                , DbDate (UTCTime (DT.fromGregorian 1921 5 1) (DT.secondsToDiffTime 34123))
+                , DbDate (DT.UTCTime (DT.fromGregorian 1921 5 1) (DT.secondsToDiffTime 34123))
               ]
 
--- 1. Write a function that filters for DbDate values and returns a list of the UTCTime values inside them.
+-- 1. Write a function that filters for DbDate values and returns a list of the DT.UTCTime values inside them.
 --
-filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate = undefined
+filterDbDate :: [DatabaseItem] -> [DT.UTCTime]
+filterDbDate xs = [i | (DbDate i) <- xs]
 
 
 -- 2. Write a function that filters for DbNumber values and returns a list of the Integer values inside them. 
 filterDbNumber :: [DatabaseItem] -> [Integer]
-filterDbNumber = undefined
+filterDbNumber xs = [i | (DbNumber) <- xs]
 
 
 -- 3. Write a function that gets the most recent date. 
-mostRecent :: [DatabaseItem] -> UTCTime
-mostRecent = undefined
+mostRecent :: [DatabaseItem] -> DT.UTCTime
+mostRecent = maximum . filterDbDate
 
 -- 4. Write a function that sums all of the DbNumber values.
 sumDb :: [DatabaseItem] -> Integer
-sumDb = undefined
+sumDb = sum . filterDbNumber
 
 
 -- 5. Write a function that gets the average of the DbNumber values.
 -- You'll probably need to use fromIntegral
 -- to get from Integer to Double.
 avgDb :: [DatabaseItem] -> Double
-avgDb = undefined
+avgDb xs = (/) ((sum . numInt) xs) ((length . numInt) xs) 
+  where
+    numInt = fromIntegral . filterDbNumber
