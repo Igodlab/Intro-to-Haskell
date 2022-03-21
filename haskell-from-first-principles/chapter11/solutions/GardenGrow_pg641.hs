395635635caa87f2e5c696f8eb2d4c6012b1583d
Igodlab <igodlab@gmail.com>
2022-03-20 18:53:51 -0700

    normal forms and more of cardinality

---
+++ haskell-from-first-principles/chapter11/solutions/GardenGrow_pg641.hs
@@ -0,0 +1,24 @@
+module GardenGrow where
+
+data FlowerType = Gardenia
+                | Daisy
+                | Rose
+                | Lilac
+                deriving Show
+
+type Gardener = String
+
+data Garden = Garden Gardener FlowerType deriving Show
+
+-- 1. What is the sum of products normal form of Garden?
+-- # Ans: Remember that the NORMAL FORM is the sum of products, so:
+--
+--        data Garden = Gardenia Gardener
+--                    | Daisy    Gardener
+--                    | Rose     Gardener
+--                    | Lilac    Gardener
+--                    deriving Show
+--
+--        Gardener has the same cardinality as String = 1114112,
+--        FlowerType has cardinality of 4 and thus
+--        Garden has cardinality = 1114112 x 4 = 4456448
