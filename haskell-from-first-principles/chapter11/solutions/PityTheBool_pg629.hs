38a97dcdb6cbc14ed9556bfa249c0e9e842849f2
Igodlab <igodlab@gmail.com>
2022-03-25 15:08:32 -0700

    data constructors, overflows and cardinality

--- haskell-from-first-principles/chapter11/solutions/PityTheBool_pg629.hs
+++ haskell-from-first-principles/chapter11/solutions/PityTheBool_pg629.hs
@@ -11,4 +11,10 @@ data NumberOrBool = Numba Int8 | BoolyBool Bool deriving (Eq, Show)
 
 myNumba = Numba (-128)
 
-
+-- 1. What is the cardinality of this datatype? Hint: We already know Bool’s cardinality. Show your work as demonstrated earlier.
+-- # Ans: Cardinality for BiggSmall is 2 + 2 = 4
+--
+--
+-- 2. What is the cardinality of NumberOrBool?
+-- # Ans: Cardinality of NumberOrBool is 2^8 + 2 = 258
+--
