0e53a40019ee51c5f33abf86aaea2fc0a3e47f1e
Igodlab <igodlab@gmail.com>
2022-04-09 22:40:35 -0700

    use dropWhile for split into sentences

--- haskell-from-first-principles/chapter11/solutions/LanguageExercises_pg695.hs
+++ haskell-from-first-principles/chapter11/solutions/LanguageExercises_pg695.hs
@@ -1,8 +1,13 @@
 module LanguageExercises where
 
+import qualified Data.Char as DC
+
 -- 1. Write a function that capitalizes a word.
 capitalizeWord :: String -> String
-capitalizeWord = undefined
+capitalizeWord (x:xs) | isAlphabet = DC.toUpper x : xs
+                      | otherwise  = x : capitalizeWord xs
+                    where
+                      isAlphabet = x `elem` ['a'..'z'] || elem x ['A'..'Z']
 
 -- Example output.
 --
@@ -14,7 +19,17 @@ capitalizeWord = undefined
 --
 -- 2. Write a function that capitalizes sentences in a paragraph. Recognize when a new sentence has begun by checking for periods. Reuse the capitalizeWord function.
 capitalizeParagraph :: String -> String
-capitalizeParagraph = undefined
+capitalizeParagraph [] = []
+capitalizeParagraph xs = (concat . map capitalizeWord) $ sentence isDot xs
+  where 
+    isDot = \x -> if x == '.' then True else False
+
+    sentence :: (Char -> Bool) -> String -> [String] 
+    sentence p x = case dropWhile p x of 
+                        "" -> []
+                        x' -> (sent ++ ['.']) : sentence p x''
+                          where
+                            (sent, x'') = break p x'
 
 -- Example result you should get from your function:
 --
