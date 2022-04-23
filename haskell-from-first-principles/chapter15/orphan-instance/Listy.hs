9180cede60d5f457bf2727144e6a6b0c08b021d9
Igodlab <igodlab@gmail.com>
2022-04-23 13:24:37 -0700

    Listy: safe multiple definitions of same typeclass instances: 1) define type, typeclass & instances in the same module. 2) if you want a new def of an instance 'wrap' the datatype with newtype

--- haskell-from-first-principles/chapter15/orphan-instance/Listy.hs
+++ haskell-from-first-principles/chapter15/orphan-instance/Listy.hs
@@ -2,10 +2,10 @@ module Listy where
 
 import qualified ListyInstances as LI
 
-newtype Listy a = LI.Listy [a] deriving (Eq, Show)
+newtype Listy a = Listy [a] deriving (Eq, Show)
 
 instance Monoid (Listy a) where
-    mempty = LI.Listy []
+    mempty = Listy []
 
 instance Semigroup (Listy a) where
-    (<>) (LI.Listy l) (LI.Listy l') = LI.Listy $ (l <> l')
+    (<>) (Listy l) (Listy l') = Listy $ (l <> l')
