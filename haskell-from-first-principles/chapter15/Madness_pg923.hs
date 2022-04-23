c6492f8f9fba1266ad401292c776c38cb2ffa214
Igodlab <igodlab@gmail.com>
2022-04-23 13:56:48 -0700

    smarter uses of mconcat for Monoidal instances

---
+++ haskell-from-first-principles/chapter15/Madness_pg923.hs
@@ -0,0 +1,40 @@
+import Data.Monoid
+
+-- "___________! he said ______ as he
+--  exclamation          adverb
+--
+-- jumped into his car ____ and drove
+--                     noun
+
+
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbin' :: Exclamation
+           -> Adverb
+           -> Noun
+           -> Adjective
+           -> String
+madlibbin' e adv noun adj = 
+    e <> "! he said " <> 
+    adv <> " as he jumped into his car " <>
+    noun <> " and drove off with his " <>
+    adj <> " wife."
+
+
+madlibbinBetter' :: Exclamation
+                 -> Adverb
+                 -> Noun
+                 -> Adjective
+                 -> String
+madlibbinBetter' e adv noun adj = mconcat ls
+  where
+    ls = [ e, "! he said "
+         , adv,  " as he jumped into his car "
+         , noun,  " and drove off with his "
+         , adj,  " wife."
+         ]
+          
