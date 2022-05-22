38ebe0371aa1408036ac903c03df26bf8298aa45
Igodlab <igodlab@gmail.com>
2022-05-22 16:16:03 -0700

    Chapter 15 done, Monoid exercises

--- haskell-from-first-principles/chapter15/solutions/MonoidExercises.hs
+++ haskell-from-first-principles/chapter15/solutions/MonoidExercises.hs
@@ -1,51 +1,33 @@
 module MonoidExercises where
 
-import           Data.Monoid
-import qualified MonoidProperty  as MP
+import           SemigroupExercises hiding (main)
+import           Data.Semigroup
+import qualified MonoidProperty     as MP
 import           Test.QuickCheck 
 
 -- prob. 1
 --
-data Trivial = Trivial deriving (Eq, Show)
-
-instance Semigroup Trivial where
-    (<>) _ _ = Trivial
-
 instance Monoid Trivial where
     mempty = Trivial
 
-instance Arbitrary Trivial where
-    arbitrary = return Trivial
-
-type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
-
 -- prob. 2
 --
-newtype Identity a = Identity a deriving (Show, Eq)
-
-instance Semigroup a => Semigroup (Identity a) where
-    Identity x <> Identity y = Identity $ x <> y
-
 instance Monoid a => Monoid (Identity a) where
     mempty = Identity mempty
 
-instance Arbitrary a => Arbitrary (Identity a) where
-    arbitrary = do 
-        Identity <$> arbitrary
-
-type IdentityAssoc = Identity String 
-                  -> Identity String
-                  -> Identity String
-                  -> Bool
-
 -- prob. 3
 -- 
-data Two a b = Two a b deriving Show
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+    mempty = Two mempty mempty
 
+type TwoIdentity = Identity (Sum Int) -> Bool 
 
--- prob. 3
+-- prob. 4
 -- 
-newtype BoolConj = BoolConj Bool
+instance Monoid BoolConj where
+    mempty = BoolConj True
+
+type BoolConjIdentity = BoolConj -> Bool 
 
 -- What it should do:
 --
@@ -56,9 +38,10 @@ newtype BoolConj = BoolConj Bool
 
 
 -- prob 5. 
-newtype BoolDisj = BoolDisj Bool 
+instance Monoid BoolDisj where
+    mempty = BoolDisj False
 
-
+type BoolDisjIdentity = BoolConj -> Bool 
 
 -- What it should do:
 -- Prelude> (BoolDisj True) `mappend` mempty
@@ -69,8 +52,9 @@ newtype BoolDisj = BoolDisj Bool
 
 
 -- prob. 6.
-newtype Combine a b = Combine { unCombine :: (a -> b) }
-
+--
+instance Monoid b => Monoid (Combine a b) where
+    mempty = Combine mempty
 
 -- What it should do:
 --
@@ -82,8 +66,8 @@ newtype Combine a b = Combine { unCombine :: (a -> b) }
 --
 -- 7. Hint: We can do something that seems a little more specific and natural to functions now that the input and output types are the same.
 --
-newtype Comp a = Comp (a -> a)
-
+instance Monoid a => Monoid (Comp a) where
+    mempty = Comp mempty
 
 -- prob. 8.
 --
@@ -91,23 +75,22 @@ newtype Comp a = Comp (a -> a)
 --
 newtype Mem s a = Mem {runMem :: s -> (a,s)}
  
- 
--- instance Monoid a => Monoid (Mem s a) where
---     mempty = undefined
+instance Semigroup a => Semigroup (Mem s a) where
+    Mem f <> Mem g = Mem h
+      where
+        h hs = (fa <> ga, gs)
+          where
+            (fa, fs) = f hs
+            (ga, gs) = g fs
+
+instance Monoid a => Monoid (Mem s a) where
+    mempty = Mem $ \hs -> (mempty, hs)
 
 -- Given the following code:
 -- 
 f' = Mem $ \s -> ("hi", s + 1)
 
 -- main2 = do
---     let rmzero = runMem mempty 0
---         rmleft = runMem (f' <> mempty) 0
---         rmright = runMem (mempty <> f') 0
---     print $ rmleft
---     print $ rmright
---     print $ (rmzero :: (String, Int))
---     print $ rmleft == runMem f' 0
---     print $ rmright == runMem f' 0
 
 -- A correct Monoid for Mem should, given the above code, get the following output:
 --
@@ -127,15 +110,32 @@ f' = Mem $ \s -> ("hi", s + 1)
 
 main :: IO ()
 main = do
-    let sa  = MP.monoidAssoc
-        mli = MP.monoidLeftIdentity
-        mlr = MP.monoidRightIdentity
     print "prob. 1. Trivial"
-    quickCheck (sa  :: TrivAssoc)
-    quickCheck (mli :: Trivial -> Bool)
-    quickCheck (mlr :: Trivial -> Bool)
+    quickCheck (semigroupAssoc         :: TrivAssoc)
+    quickCheck (MP.monoidLeftIdentity  :: Trivial -> Bool)
+    quickCheck (MP.monoidRightIdentity :: Trivial -> Bool)
     print "prob 2. Identity"
-    quickCheck (MP.monoidAssoc         :: IdentityAssoc)
+    quickCheck (semigroupAssoc         :: IdentityAssoc)
     quickCheck (MP.monoidLeftIdentity  :: Identity String -> Bool)
     quickCheck (MP.monoidRightIdentity :: Identity String -> Bool)
-
+    print "prob 3. Two"
+    quickCheck (semigroupAssoc         :: TwoAssoc)
+    quickCheck (MP.monoidLeftIdentity  :: TwoIdentity)
+    quickCheck (MP.monoidRightIdentity :: TwoIdentity)
+    print "prob 4. BoolConj"
+    quickCheck (semigroupAssoc         :: BoolConjAssoc)
+    quickCheck (MP.monoidLeftIdentity  :: BoolConjIdentity )
+    quickCheck (MP.monoidRightIdentity :: BoolConjIdentity )
+    print "prob 5. BoolDisj"
+    quickCheck (semigroupAssoc         :: BoolDisjAssoc)
+    quickCheck (MP.monoidLeftIdentity  :: BoolDisjIdentity)
+    quickCheck (MP.monoidRightIdentity :: BoolDisjIdentity)
+    print "prob 8. Mem"
+    let rmzero = runMem mempty 0
+        rmleft = runMem (f' <> mempty) 0
+        rmright = runMem (mempty <> f') 0
+    print $ rmleft
+    print $ rmright
+    print $ (rmzero :: (String, Int))
+    print $ rmleft == runMem f' 0
+    print $ rmright == runMem f' 0
