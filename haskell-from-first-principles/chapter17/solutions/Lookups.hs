8d6244a516e84b140ed15389172c0cd57684a6cc
Igodlab <igodlab@gmail.com>
2022-06-11 13:09:48 -0700

    short exercises: lookups

--- haskell-from-first-principles/chapter17/solutions/Lookups.hs
+++ haskell-from-first-principles/chapter17/solutions/Lookups.hs
@@ -2,7 +2,7 @@ module Lookups where
 
 import Data.List (elemIndex)
 
-
+import GHC.Base (liftA2)
 
 -- In the following exercises you will need to use the following terms to make the expressions typecheck:
 --
@@ -12,47 +12,49 @@ import Data.List (elemIndex)
 -- 3. (<*>)
 -- Make the following expressions typecheck.
 --
--- Make the following expressions typecheck.
---
 --1. 
 added :: Maybe Integer
-added = (+3) (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+added = fmap (+3) (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
 
 
 -- 2. 
-y :: Maybe Integer
-y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+y2 :: Maybe Integer
+y2 = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
 
-z :: Maybe Integer
-z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+z2 :: Maybe Integer
+z2 = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
 
 tupled :: Maybe (Integer, Integer)
-tupled = (,) y z
+tupled = (,) <$> y2 <*> z2
 
+tupled' :: Maybe (Integer, Integer)
+tupled' = liftA2 (,) y2 z2
 
 -- 3. 
-x :: Maybe Int
-x = elemIndex 3 [1, 2, 3, 4, 5]
+x3 :: Maybe Int
+x3 = elemIndex 3 [1, 2, 3, 4, 5]
 
-y :: Maybe Int
-y = elemIndex 4 [1, 2, 3, 4, 5]
+y3 :: Maybe Int
+y3 = elemIndex 4 [1, 2, 3, 4, 5]
 
 max' :: Int -> Int -> Int
 max' = max
 
 maxed :: Maybe Int
-maxed = max' x y
+maxed = max' <$> x3 <*> y3
 
+maxed' :: Maybe Int
+maxed' = liftA2 max' x3 y3
 
 -- 4. 
 xs = [1, 2, 3]
 ys = [4, 5, 6]
 
-x :: Maybe Integer
-x = lookup 3 $ zip xs ys
+x4 :: Maybe Integer
+x4 = lookup 3 $ zip xs ys
 
-y :: Maybe Integer
-y = lookup 2 $ zip xs ys
+y4 :: Maybe Integer
+y4 = lookup 2 $ zip xs ys
 
 summed :: Maybe Integer
-summed = sum $ (,) x y
+summed = fmap sum $ (,) <$> x4 <*> y4
