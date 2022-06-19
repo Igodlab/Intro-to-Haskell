c3faad88099926cf0da4bf9aecd600ebdd6c9945
Igodlab <igodlab@gmail.com>
2022-06-18 17:25:00 -0700

    Constant Instance, needs Monoid and Semigroup

--- haskell-from-first-principles/chapter17/solutions/ConstantInstance.hs
+++ haskell-from-first-principles/chapter17/solutions/ConstantInstance.hs
@@ -5,6 +5,6 @@ newtype Constant a b = Constant {getConstant :: a} deriving (Eq, Ord, Show)
 instance Functor (Constant a) where
     fmap _ (Constant a) = Constant a 
 
-instance Monoid a => Applicative (Constant a) where
+instance Semigroup a => Applicative (Constant a) where
     pure _ = Constant mempty 
     Constant x <*> Constant y = Constant $ x <> y 
