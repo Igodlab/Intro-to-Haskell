c96d8e89b6ed6ea2afbae1973542dd60aae20ce7
Igodlab <igodlab@gmail.com>
2022-11-26 19:14:05 -0800

    incomplete exercise, stuck at defining Applicative instance for Validation

---
+++ haskell-from-first-principles/chapter17/solutions/VariationsOnEither.hs
@@ -0,0 +1,27 @@
+#!usr/bin/env cabal
+{- cabal:
+build-depends:  base
+              , QuickCheck
+              , checkers
+              , semigroupoids
+-}
+
+import Data.Functor.Bind.Class 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+data Validation' e a = Failure' e | Success' a deriving (Eq, Show)
+
+instance Functor (Validation' e) where
+    fmap _ (Failure' e) = Failure' e
+    fmap f (Success' a) = Success' $ f a
+
+instance Semigroup e => Applicative (Validation' e) where
+    pure = Success'
+    (<*>) = (<.>)
+
+main :: IO ()
+main = do
+    putStrLn "List tests"
+    quickBatch $ functor (undefined :: Validation' (Int) (Int))
