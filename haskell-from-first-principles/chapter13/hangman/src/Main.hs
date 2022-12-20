dc4f02cc2406eb0497c01f2464caaba8f7c9ce04
Igodlab <igodlab@gmail.com>
2022-12-20 15:40:18 -0800

    hangman game ready!

--- haskell-from-first-principles/chapter13/hangman/src/Main.hs
+++ haskell-from-first-principles/chapter13/hangman/src/Main.hs
@@ -11,29 +11,35 @@ import System.Random (randomRIO) -- [6]
 
 main :: IO ()
 main = do
-    undefined
+    word <- randomWord'
+    runGame $ puzzle word
+  where
+    puzzle w = freshPuzzle (fmap toLower w)
 
 
-type WordList = [String]
+newtype WordList = WordList [String] deriving (Eq, Show)
 
 allWords :: IO WordList
 allWords = do 
-    dict <- readFile "../data/dict.txt"
-    return (lines dict)
+    dict <- readFile "data/dict.txt"
+    return $ WordList (lines dict)
 
-minWordLen = 5 :: Int
-maxWordLen = 9 :: Int
+minWordLen :: Int
+minWordLen = 4
+
+maxWordLen :: Int
+maxWordLen = 9
 
 gameWords :: IO WordList 
 gameWords = do 
-    aw <- allWords
-    return (filter gameLen aw)
+    (WordList aw) <- allWords
+    return $ WordList (filter gameLen aw)
       where
         gameLen w = let l = length (w :: String)
                     in l >= minWordLen && l < maxWordLen 
 
 randomWord :: WordList -> IO String
-randomWord wl = do 
+randomWord (WordList wl) = do 
     randIx <- randomRIO (0, length wl - 1)
     return $ wl !! randIx
 
@@ -43,18 +49,72 @@ randomWord' = gameWords >>= randomWord
 data Puzzle = Puzzle String [Maybe Char] [Char]
 
 instance Show Puzzle where
-    show (Puzzle _ discovered guessed) = (intersperse ' ' $ (fmap renderPuzzleChar discovered) ++ "Guessed so far: " ++ guessed)
+    show (Puzzle _ discovered guessed) = (intersperse ' ' $ fmap renderPuzzleChar discovered) ++ "\nGuessed so far: " ++ guessed
 
 freshPuzzle :: String -> Puzzle
-freshPuzzle = undefined 
+freshPuzzle st = let lw  = length st
+                     rep = replicate lw Nothing
+                 in Puzzle st rep []
 
-chainInWord :: Puzzle -> Char -> Bool
-chainInWord = undefined 
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle st _ _) c = elem c st
 
-alreadyGuessed :: String -> Char -> Bool
-alreadyGuessed = undefined
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guess) c = elem c guess
 
 renderPuzzleChar :: Maybe Char -> Char 
-renderPuzzleChar = undefined
+renderPuzzleChar Nothing = '_'
+renderPuzzleChar (Just c) = c
 
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle st disc guess) c = Puzzle st newFilledInSoFar (c:guess)
+  where
+    zipper guessed wordChar guessChar = 
+        if wordChar == guessed 
+        then Just wordChar
+        else guessChar
+
+    newFilledInSoFar = zipWith (zipper c) st disc
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+    putStrLn $ "Your guess was: " ++ [guess]
+    case (charInWord puzzle guess
+        , alreadyGuessed puzzle guess) of
+            (_, True) -> do
+                putStrLn "You already guessed that character, pick something else!"
+                return puzzle
+            (True, _) -> do
+                putStrLn "This character was in the word, filling in the word accordingly"
+                return (fillInCharacter puzzle guess)
+            (False, _) -> do
+                putStrLn "This character wasn't in the word, try again."
+                return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+    if (length guessed) > 17 then
+        do putStrLn "You lose!"
+           putStrLn $ "The word was: " ++ wordToGuess
+           exitSuccess
+    else return ()
+
+gameWin :: Puzzle -> IO ()
+gameWin puzzle@(Puzzle _ filledInSoFar _) = 
+    if all isJust filledInSoFar then
+        do putStrLn $ "Completed puzzle is: " ++ show puzzle
+           putStrLn "You WIN!!!"
+           exitSuccess 
+    else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do 
+    gameOver puzzle
+    gameWin puzzle
+    putStrLn $ "Current puzzle is: " ++ show puzzle
+    putStr "Guess a letter: "
+    guess <- getLine
+    case guess of 
+        [c] -> handleGuess puzzle c >>= runGame
+        _   -> putStrLn "Your guess must be a single character"
 
