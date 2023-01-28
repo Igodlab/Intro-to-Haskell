d115f71a9088aa394d36a56f52904d4326d43aa6
Igodlab <igodlab@gmail.com>
2023-01-28 13:54:15 -0800

    Palindrome filter improved

--- haskell-from-first-principles/chapter13/solutions/Palindrome.hs
+++ haskell-from-first-principles/chapter13/solutions/Palindrome.hs
@@ -8,13 +8,8 @@ palindrome :: IO ()
 palindrome = forever $ do
     line1 <- getLine
     let lowCap = map toLower line1 
-        clean = cleanLn1 lowCap
+        clean = filter isAlpha lowCap
     case (clean == reverse clean) of
         True -> putStrLn "It's a palindrome!"
         False -> do putStrLn "Nope!"
                     exitSuccess
-  where
-    cleanLn1 [] = []
-    cleanLn1 (x:xs) | not $ isAlpha x = cleanLn1 xs
-                    | otherwise       = x : cleanLn1 xs
-
