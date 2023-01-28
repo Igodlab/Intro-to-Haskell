cbbdc4f3a2d7971720c527abfdd085cf415ebe28
Igodlab <igodlab@gmail.com>
2023-01-28 13:51:46 -0800

    Palindrome exercise completed, including filter of special characters

--- haskell-from-first-principles/chapter13/solutions/Palindrome.hs
+++ haskell-from-first-principles/chapter13/solutions/Palindrome.hs
@@ -1,13 +1,20 @@
 module Palindromes where
 
 import Control.Monad
+import Data.Char (isAlpha, toLower)
 import System.Exit 
 
 palindrome :: IO ()
 palindrome = forever $ do
     line1 <- getLine
-    case (line1 == reverse line1) of
+    let lowCap = map toLower line1 
+        clean = cleanLn1 lowCap
+    case (clean == reverse clean) of
         True -> putStrLn "It's a palindrome!"
         False -> do putStrLn "Nope!"
                     exitSuccess
+  where
+    cleanLn1 [] = []
+    cleanLn1 (x:xs) | not $ isAlpha x = cleanLn1 xs
+                    | otherwise       = x : cleanLn1 xs
 
