6efc816735b2d4f62385e29b57d721c60da547e0
Igodlab <igodlab@gmail.com>
2023-01-28 13:35:52 -0800

    Palindrome IO with exit when False, finished

--- haskell-from-first-principles/chapter13/solutions/Palindrome.hs
+++ haskell-from-first-principles/chapter13/solutions/Palindrome.hs
@@ -1,9 +1,13 @@
+module Palindromes where
+
 import Control.Monad
+import System.Exit 
 
 palindrome :: IO ()
 palindrome = forever $ do
-line1 <- getLine
-case (line1 == reverse line1) of
-True -> putStrLn "It's a palindrome!"
-False -> putStrLn "Nope!"
+    line1 <- getLine
+    case (line1 == reverse line1) of
+        True -> putStrLn "It's a palindrome!"
+        False -> do putStrLn "Nope!"
+                    exitSuccess
 
